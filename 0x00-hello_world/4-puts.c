45edd78a3f8f23af5fd533c68bb364c8a285ac46
Uzezioke <uzeziotuorimuo@gmail.com>
2022-09-09 07:06:16 -0700

    Task 4 redone

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 
 /**
+ * * main - Entry point
+ * *
+ * * Return: Always 0 (Success)
  *
- *  * Description: main - Print out a string to stdout.
  *
- *   * Return: 0 if successfull.
- *
- *    */
-
+ * /
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
