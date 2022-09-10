74ff19486f8a8ba089c5e3d7ec0c6dc7ca96075f
Uzezioke <uzeziotuorimuo@gmail.com>
2022-09-09 17:56:43 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int 1, j, k;
+
+	i = 0;
+
+	while (1 < 100)
+	{
+		j = i % 10;
+		k = i /10;
+
+		if (k < j)
+		{
+			putchar(k + '0');
+			putchar(j + '0');
+
+			if (i < 89)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
