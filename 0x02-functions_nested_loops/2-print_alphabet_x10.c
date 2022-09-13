7200976fa2a9401035be7dabeca301e1d9984421
Uzezioke <uzeziotuorimuo@gmail.com>
2022-09-13 10:27:39 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *
+ * print_alphabet_x10 - prints 10 times the alphabet in lower cals
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char i;
+
+	char h;
+
+	for (h = 0; h <= 9; h++)
+	{
+		for (i = 'a'; i <= 'z' i++)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
