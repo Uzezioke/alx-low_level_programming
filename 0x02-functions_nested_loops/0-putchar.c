11f4cad2285c60744a9cd7ba078ef0a420e5cd9f
Uzezioke <uzeziotuorimuo@gmail.com>
2022-09-13 09:28:28 -0700

    Task 0 new

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 /**
  * main - print _putchar
  * Return: Always 0 (Success)
@@ -14,6 +14,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
