a3291f3abb8cb301abf58bcd41e8b1fc13252efa
Uzezioke <uzeziotuorimuo@gmail.com>
2022-09-15 17:08:14 -0700

    Task 4 update

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,7 +12,9 @@ void print_most_numbers(void)
 
 	for (number = '0'; number <= '9'; number++)
 	{
-		if (number == '2' || number == '4');
+		if (number == '2' || number == '4')
+			;
+
 		else
 			_putchar(number);
 	}
