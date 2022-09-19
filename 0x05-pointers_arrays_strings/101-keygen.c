65e991a3ac8eca0384ab25dde3814b0429b074f5
Uzezioke <uzeziotuorimuo@gmail.com>
2022-09-19 12:11:17 -0700

    Task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,23 +9,23 @@
 
 int main(void)
 {
-	srand((unsigned int)time(NULL));
-	char pass[13];
-	int i;
+srand((unsigned int)time(NULL));
+char pass[13];
+int i;
 
-	for (i = 0; i < 4; i++)
-	{
+for (i = 0; i < 4; i++)
+{
 
-		pass[3 * i] = '0' + (rand() % 10);
-		char capLetter = 'A' + (rand() % 26);
+pass[3 * i] = '0' + (rand() % 10);
+char capLetter = 'A' + (rand() % 26);
 
-		pass[(3 * i) + 1] = capLetter;
-		char letter = 'a' + (rand() % 26);
-	}
-		pass[3 * i] = '\0';
-		printf("generated password : %s\n\n", pass);
+pass[(3 * i) + 1] = capLetter;
+char letter = 'a' + (rand() % 26);
+}
+pass[3 * i] = '\0';
+printf("generated password : %s\n\n", pass);
 
-		printf("\n\n");
+printf("\n\n");
 
-		return (0);
+return (0);
 }
