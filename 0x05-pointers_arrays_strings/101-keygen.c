d7b8be707705c736e3f0ab5ad5f7833e0fdb5749
Uzezioke <uzeziotuorimuo@gmail.com>
2022-09-19 14:08:47 -0700

    Task 11 new

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -17,7 +17,7 @@ int main(void)
 	for (count = 0, total = 2772; total > 122; count++)
 	{
 		myrand = (rand() % 125) + 1;
-		printf*"%c", myrand);
+		printf("%c", myrand);
 		total -= myrand;
 	}
 	print("%c", total);
