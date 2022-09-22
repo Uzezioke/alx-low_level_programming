1a45bc384b653b510284b1309ae7014f3f4660da
Uzezioke <uzeziotuorimuo@gmail.com>
2022-09-21 18:47:15 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,17 +13,20 @@ char *_strcat(char *dest, char *src)
 	int j;
 
 	i = 0;
-	while (dest[i] != '\0')
-	{
-		j++;
-	}
+
 	j = 0;
-	while (src[j] != '\0')
+	while (*(dest + i) != '\0')
 	{
-		dest[i] = src[j];
 		i++;
 		j++;
 	}
-	dest[i] = '\0';
+	i = 0;
+	while (*(src + i) != '\0')
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	dest[j] = '\0';
 	return (dest);
 }
