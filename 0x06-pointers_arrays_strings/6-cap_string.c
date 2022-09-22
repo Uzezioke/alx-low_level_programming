57d0753c2b96fb71995d10de6a2655132bb022b0
Uzezioke <uzeziotuorimuo@gmail.com>
2022-09-21 20:46:35 -0700

    Remaining tasks

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * cap_string - converts small letters to capital letters
+ * @s: checked
+ * Return: s
+ */
+
+char *cap_string(char *s)
+{
+	int i, j, separatorFound;
+
+	for (i = 0; s[i]; ++i)
+	{
+		if (separatorFound && s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+		separatorFound = 0;
+		for (j = 0; j < 12; j++)
+		{
+			if (s[i] == '\t' || s[i] == '\n' || s[i] == ',' || s[i] == '\"' || s[i] == '-' || s[i] == '!' || s[i] == '{' || s[i] == '}' || s[i] == '(' || s[i] == ')' || s[i] == ' ' || s[i] == '?')
+			{
+				separatorFound = 1;
+			}
+		}
+	}
+	return (s);
+}
