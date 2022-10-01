f54793ec5f92f7dd133ec80b182c7ecea19e8b2b
Uzezioke <uzeziotuorimuo@gmail.com>
2022-09-30 18:32:03 -0700

    Task 0

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcat - Function that concatenates two strings.
+ * @src: cadena a copiar
+ * @dest: destino de la cadena.
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+
+	j = 0;
+	while (*(dest + i) != '\0')
+	{
+		i++;
+		j++;
+	}
+	i = 0;
+	while (*(src + i) != '\0')
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	dest[j] = '\0';
+	return (dest);
+}
---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - counts string except \0
+ * @s: checked
+ * Return: i
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		;
+	}
+	return (i);
+
+}
